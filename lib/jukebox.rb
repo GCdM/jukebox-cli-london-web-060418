bc77cf69cd14000102cd8707f962d9c104e66cd5
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-28 16:29:30 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,7 +30,6 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
   valid_num = Array.new(songs.length) {|i| (i + 1).to_s}
-  
   if valid_num.include?(user_input)
     puts "Playing ##{songs[user_input.to_i - 1]}"
   elsif songs.include?(user_input)
@@ -38,4 +37,8 @@ def play(songs)
   else
     puts "Invalid input, please try again"
   end
+end
+
+def exit_jukebox
+  
 end
